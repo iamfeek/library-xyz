29be3a1a713137639bf33f7b26fc66b918deffa0
Feek <iamfeek@users.noreply.github.com>
2023-12-02 04:51:51 +0000

    Add margin bottom to LayoutHeader

--- app/LayoutHeader.tsx
+++ app/LayoutHeader.tsx
@@ -9,7 +9,7 @@ export const LayoutHeader = () => {
 
   return (
     <header>
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mb-4">
         <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">
           {currentRoute ? currentRoute.name : "Library XYZ"}
         </h1>
